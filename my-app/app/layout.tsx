81eb05e409c0630e284b0b0834c0a3cc82fbca83
codedbyelif <elifkyr3@gmail.com>
2026-01-21 13:36:39 +0300

    feat : splash crsor added .

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SplashCursor from "../components/SplashCursor";
 
 
 
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SplashCursor />
 
       </body>
     </html>
