42d5898ca94a5a160e98390a667e0f9d1a9da5ac
codedbyelif <elifkyr3@gmail.com>
2026-01-23 18:28:09 +0300

    style : feature section changed

--- components/FeatureSection.tsx
+++ components/FeatureSection.tsx
@@ -16,13 +16,6 @@ const FeatureSection = () => {
                 </div>
                 <div className="grid gap-6 md:grid-cols-2 lg:gap-8">
                     <div className="flex flex-col overflow-clip rounded-xl border border-white/10 md:col-span-2 md:grid md:grid-cols-2 md:gap-6 lg:gap-8 bg-white/5">
-                        <div className="md:min-h-[24rem] lg:min-h-[28rem] xl:min-h-[32rem]">
-                            <img
-                                alt="Feature 1"
-                                className="aspect-16/9 h-full w-full object-cover object-center"
-                                src="https://shadcnblocks.com/images/block/placeholder-1.svg"
-                            />
-                        </div>
                         <div className="flex flex-col justify-center px-6 py-8 md:px-8 md:py-10 lg:px-10 lg:py-12">
                             <h3 className="mb-3 text-lg font-semibold md:mb-4 md:text-2xl lg:mb-6">
                                 Feature 1
@@ -31,8 +24,22 @@ const FeatureSection = () => {
                                 Nam vitae molestie arcu. Quisque eu libero orci. Aliquam imperdiet magna nec massa consectetur, id interdum ante congue.
                             </p>
                         </div>
+                        <div className="md:min-h-[24rem] lg:min-h-[28rem] xl:min-h-[32rem]">
+                            <img
+                                alt="Feature 1"
+                                className="aspect-16/9 h-full w-full object-cover object-center"
+                                src="https://shadcnblocks.com/images/block/placeholder-1.svg"
+                            />
+                        </div>
                     </div>
-                    <div className="flex flex-col-reverse overflow-clip rounded-xl border border-white/10 md:col-span-2 md:grid md:grid-cols-2 md:gap-6 lg:gap-8 bg-white/5">
+                    <div className="flex flex-col overflow-clip rounded-xl border border-white/10 md:col-span-2 md:grid md:grid-cols-2 md:gap-6 lg:gap-8 bg-white/5">
+                        <div className="md:min-h-[24rem] lg:min-h-[28rem] xl:min-h-[32rem]">
+                            <img
+                                alt="Feature 2"
+                                className="aspect-16/9 h-full w-full object-cover object-center"
+                                src="https://shadcnblocks.com/images/block/placeholder-2.svg"
+                            />
+                        </div>
                         <div className="flex flex-col justify-center px-6 py-8 md:px-8 md:py-10 lg:px-10 lg:py-12">
                             <h3 className="mb-3 text-lg font-semibold md:mb-4 md:text-2xl lg:mb-6">
                                 Feature 2
@@ -40,13 +47,6 @@ const FeatureSection = () => {
                             <p className="text-muted-foreground lg:text-lg">
                                 Nam vitae molestie arcu. Quisque eu libero orci. Aliquam imperdiet magna nec massa consectetur, id interdum ante congue.
                             </p>
-                        </div>
-                        <div className="md:min-h-[24rem] lg:min-h-[28rem] xl:min-h-[32rem]">
-                            <img
-                                alt="Feature 2"
-                                className="aspect-16/9 h-full w-full object-cover object-center"
-                                src="https://shadcnblocks.com/images/block/placeholder-2.svg"
-                            />
                         </div>
                     </div>
                 </div>
